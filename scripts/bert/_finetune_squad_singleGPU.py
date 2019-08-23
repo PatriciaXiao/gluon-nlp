c4333d7e11db8094f3311e9ca9eafdd9fd41d327
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-23 15:51:32 -0700

    Update _finetune_squad_singleGPU.py

--- scripts/bert/_finetune_squad_singleGPU.py
+++ scripts/bert/_finetune_squad_singleGPU.py
@@ -348,7 +348,7 @@ if args.apply_coattention:
     net.co_attention.collect_params().initialize(ctx=ctx)
 
 if args.apply_self_attention:
-    net.multi_head_attention.initialize(ctx=ctx)
+    net.multi_head_attention.collect_params().initialize(ctx=ctx)
 
 net.hybridize(static_alloc=True)
 
