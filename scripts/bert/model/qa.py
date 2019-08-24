2255840e719764df4be7f217333bb5e0629a92c6
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-23 18:45:45 -0700

    debug

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -247,6 +247,10 @@ class BertForQA(Block):
             context_mask = mx.ndarray.squeeze(context_mask, axis=(0, 1))[:,:context_max_len]
             query_mask = query_mask[:,:query_max_len]
             # get the two encodings separated
+            a = mx.ndarray.squeeze(warpped_out, axis=0)
+            print(a)
+            print(a[:,:,:context_max_len])
+            exit(0)
             context_emb_encoded = mx.ndarray.transpose(mx.ndarray.squeeze(warpped_out, axis=0)[:,:,:context_max_len], axes=(1,2,0))
             query_emb_encoded = mx.ndarray.transpose(mx.nd.multiply(query_mask, o), axes=(1,2,0))[:,:,query_max_len]
             attended_output = self.co_attention(bert_output, query_emb_encoded, 
