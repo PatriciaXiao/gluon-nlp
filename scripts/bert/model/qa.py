6e1eb9042672cfbf96ebc5d99705c7a6b479689b
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-23 18:42:58 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -230,6 +230,8 @@ class BertForQA(Block):
             o = mx.ndarray.transpose(bert_output, axes=(2,0,1))
             context_mask = token_types
             query_mask = 1 - context_mask
+            context_max_len = int(context_mask.sum(axis=1).max().asscalar())
+            query_max_len = int(query_mask.sum(axis=1).max().asscalar())
             context_raw = mx.nd.multiply(context_mask, o)
             context_raw = mx.ndarray.expand_dims(context_raw, 0)
             # to get the offset to shift using gridgenerator and bilinear-sampler
@@ -242,12 +244,11 @@ class BertForQA(Block):
             context_mask = mx.ndarray.expand_dims(context_mask, 0)
             context_mask = mx.ndarray.expand_dims(context_mask, 0)
             context_mask = BilinearSampler(context_mask, grid)
-            context_mask = mx.ndarray.squeeze(context_mask, axis=(0, 1))
+            context_mask = mx.ndarray.squeeze(context_mask, axis=(0, 1))[:,:context_max_len]
+            query_mask = query_mask[:,:query_max_len]
             # get the two encodings separated
-            context_emb_encoded = mx.ndarray.transpose(mx.ndarray.squeeze(warpped_out, axis=0), axes=(1,2,0))
-            query_emb_encoded = mx.ndarray.transpose(mx.nd.multiply(query_mask, o), axes=(1,2,0))
-            context_max_len = int(context_mask.sum(axis=1).max().asscalar())
-            query_max_len = int(query_mask.sum(axis=1).max().asscalar())
+            context_emb_encoded = mx.ndarray.transpose(mx.ndarray.squeeze(warpped_out, axis=0)[:,:,:context_max_len], axes=(1,2,0))
+            query_emb_encoded = mx.ndarray.transpose(mx.nd.multiply(query_mask, o), axes=(1,2,0))[:,:,query_max_len]
             attended_output = self.co_attention(bert_output, query_emb_encoded, 
                                                 context_mask, query_mask, 
                                                 context_max_len, query_max_len)
