2fb3d7a2b5aecfc021ac27b598d454d10d3f3527
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-24 18:10:47 -0700

    Update finetune_squad.py

--- scripts/bert/finetune_squad.py
+++ scripts/bert/finetune_squad.py
@@ -391,7 +391,8 @@ def train():
 
     # refer to evaluation process
     for feat in train_dataset:
-        print(feat.tokens, feat.token_to_orig_map)
+        print(feat.tokens)
+        print(feat.token_to_orig_map)
     exit(0)
 
     #for line in train_data_transform:
