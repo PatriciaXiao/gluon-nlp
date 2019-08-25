554d0cd431be1a40e56dbb4529b16834113b27af
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-25 16:19:15 -0700

    Update verify.py

--- scripts/bert/verify.py
+++ scripts/bert/verify.py
@@ -1,11 +1,11 @@
 import mxnet as mx
 
 class AnswerVerify(object):
-	def __init__(self, name='simple'):
-		self.name = name
+    def __init__(self, name='simple'):
+        self.name = name
 
-	def train(self, train_features, example_ids, out):
-		output = mx.nd.split(out, axis=2, num_outputs=2)
+    def train(self, train_features, example_ids, out):
+        output = mx.nd.split(out, axis=2, num_outputs=2)
         example_ids = example_ids.asnumpy().tolist()
         pred_start = output[0].reshape((0, -3)).asnumpy()
         pred_end = output[1].reshape((0, -3)).asnumpy()
