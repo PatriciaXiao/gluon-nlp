f170b5f481c772076b8ab3d7e37a8bacdb1e04ce
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-25 19:56:08 -0700

    debug

--- scripts/bert/verify.py
+++ scripts/bert/verify.py
@@ -3,6 +3,18 @@ import gluonnlp as nlp
 
 from bert_qa_evaluate import PredResult, predict
 
+from mxnet.gluon.data.dataset import Dataset
+
+import model
+
+class VerifierDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+    def __getitem__(self, idx):
+        return self.data[idx]
+    def __len__(self):
+        return len(self.data)
+
 class AnswerVerify(object):
     def __init__(self, tokenizer=nlp.data.BERTBasicTokenizer(lower=True),
                 max_answer_length=30,
@@ -16,16 +28,22 @@ class AnswerVerify(object):
         self.version_2=version_2
 
     def train(self, train_features, example_ids, out):
-        self.parse_sentences(train_features, example_ids, out)
+        dataset = self.parse_sentences(train_features, example_ids, out)
+        print(len(dataset))
+        exit(0)
 
     def parse_sentences(self, train_features, example_ids, out):
         output = mx.nd.split(out, axis=2, num_outputs=2)
         example_ids = example_ids.asnumpy().tolist()
         pred_start = output[0].reshape((0, -3)).asnumpy()
         pred_end = output[1].reshape((0, -3)).asnumpy()
+        raw_data = []
         for example_id, start, end in zip(example_ids, pred_start, pred_end):
             results = [PredResult(start=start, end=end)]
             features = train_features[example_id]
+            label = 0 if features[0].is_impossible else 1
+            # if features[0].is_impossible:
+            #     prediction = ""
             prediction, _ = predict(
                 features=features,
                 results=results,
@@ -34,11 +52,13 @@ class AnswerVerify(object):
                 null_score_diff_threshold=self.null_score_diff_threshold,
                 n_best_size=self.n_best_size,
                 version_2=self.version_2)
-            print("context:", ' '.join(features[0].doc_tokens)) # the original context
-            print("question:", features[0].question_text)
-            print("prediction:", prediction)
-            print("unanswerable:", features[0].is_impossible)
-            if features[0].is_impossible:
-                exit(0)
+            # print("context:", ' '.join(features[0].doc_tokens)) # the original context
+            # print("question:", features[0].question_text)
+            # print("prediction:", prediction)
+            # print("unanswerable:", features[0].is_impossible)
+            question_text = features[0].question_text
+            raw_data.append([question_text, prediction, label])
+        dataset = VerifierDataset(raw_data)
+        return dataset
 
 
