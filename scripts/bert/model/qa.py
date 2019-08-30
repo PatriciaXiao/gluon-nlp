71d844a9d66e7f7fdecd5c3e0fd5a99a03aaf2d6
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-29 17:44:53 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -286,8 +286,6 @@ class BertForQA(Block):
             attended_output, att_weights = self.multi_head_attention(bert_output, bert_output)   
         if self.apply_transformer:
             attended_output, additional_outputs = self.transformer(bert_output)
-            print(attended_output)
-            exit(0)
         if self.add_query or self.apply_self_attention or self.apply_coattention or self.apply_transformer:
             output = self.span_classifier(attended_output)
         else:
