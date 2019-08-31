10a81d5f73ddeeee559608612bcd33ab9bd019d6
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-31 16:26:11 -0700

    Update verify.py

--- scripts/bert/verify.py
+++ scripts/bert/verify.py
@@ -343,9 +343,6 @@ class AnswerVerify(object):
             first_part = sentence_text + '. ' + question_text
             second_part = prediction
             raw_data.append([first_part, second_part, label])
-            print(first_part)
-            print(second_part)
-            print(label)
         dataset_raw = VerifierDataset(raw_data)
         dataset = dataset_raw.transform(self.transform)
         train_sampler = nlp.data.FixedBucketSampler(lengths=[int(item[1]) for item in dataset],
