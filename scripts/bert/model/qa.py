5ebee43baff932ff6eaf2ea11ec8a265abefaaa4
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-02 16:05:50 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -122,7 +122,7 @@ class CoAttention(Block):
         q2c = F.batch_dot(F.batch_dot(
             similarity_dash, similarity_dash_trans), context)
         if cls_emb_encoded is not None:
-            cls_reshaped = self.cls_mapping(cls_emb_encoded)
+            cls_reshaped = self.cls_mapping(cls_emb_encoded).reshape((context.shape[0], 1, context.shape[2]))
             zeros = mx.nd.zeros((context.shape[0], context.shape[1] - 1, context.shape[2])).as_in_context(ctx)
             cls_added = mx.ndarray.concat(cls_reshaped, zeros, dim=1).as_in_context(ctx)
             print(cls_added)
