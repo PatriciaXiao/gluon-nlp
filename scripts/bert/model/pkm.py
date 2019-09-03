98508b69395c867582885f0fe97208afe34d0603
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-02 18:40:45 -0700

    Update pkm.py

--- scripts/bert/model/pkm.py
+++ scripts/bert/model/pkm.py
@@ -46,6 +46,25 @@ class HashingMemory(Block):
         self.value_dropout = params.value_dropout
         # initialize keys / values
         self.initialize_keys()
+        # no embedding-bag available, thus need to sum it up latter
+        self.values = nn.Embedding(self.size, self.v_dim)
+        # query network
+        self.query_proj = nn.HybridSequential()
+        with self.name_scope():
+            self.query_proj.add(nn.Dense(units=self.heads * self.k_dim,
+                                        in_units=self.input_dim,
+                                        use_bias=True))
+            if params.query_batchnorm:
+                self.query_proj.add(nn.BatchNorm(center=True))
+
+        if params.query_batchnorm:
+            print("WARNING: Applying batch normalization to queries improves the performance "
+                  "and memory usage. But if you use it, be sure that you use batches of "
+                  "sentences with the same size at training time (i.e. without padding). "
+                  "Otherwise, the padding token will result in incorrect mean/variance "
+                  "estimations in the BatchNorm layer.\n")
+
+
     def initialize_keys(self):
         """
         Create two subkey sets per head.
@@ -59,6 +78,18 @@ class HashingMemory(Block):
                 for j in range(2)
             ])).reshape(self.heads, 2, self.n_keys, half))
         self.keys = gluon.Parameter(keys)
+
+    def forward(self, input):
+        """
+        Read from the memory.
+        """
+        # input dimensions
+        assert input.shape[-1] == self.input_dim
+        prefix_shape = input.shape[:-1]
+        bs = np.prod(prefix_shape)
+
+        print(bs)
+        exit(0)
 
 params = AttrDict({
     "sparse": False,
@@ -80,7 +111,13 @@ if __name__ == "__main__":
     output_dim = 100
     memory = HashingMemory(input_dim, output_dim, params)
     # net.collect_params()
-    memory.initialize(init=mx.init.Xavier(), ctx=ctx)
+    memory.initialize(init=mx.init.Normal(output_dim ** -0.5), ctx=ctx)
+    memory.hybridize()
+    print(memory)
+    # the input
+    x = mx.ndarray.random.randn(2, 3, 4, input_dim).as_in_context(ctx)
+    output = memory(x)
+
 
 
 
