c115a7174626c7fd3a4cca686b4ab480f1ff2590
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-02 17:48:25 -0700

    Create pkm.py

---
+++ scripts/bert/model/pkm.py
@@ -0,0 +1,26 @@
+# product-key memory
+# translated from https://github.com/facebookresearch/XLM/blob/master/PKM-layer.ipynb
+
+import mxnet as mx
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+params = AttrDict({
+    "sparse": False,
+    "k_dim": 128,
+    "heads": 4,
+    "knn": 32,
+    "n_keys": 512,  # the memory will have (n_keys ** 2) values
+    "query_batchnorm": True,
+    "input_dropout": 0,
+    "query_dropout": 0,
+    "value_dropout": 0,
+})
+
+if __name__ == "__main__":
+	ctx = mx.gpu(0) if mx.context.num_gpus() else mx.cpu()
+	print(ctx)
