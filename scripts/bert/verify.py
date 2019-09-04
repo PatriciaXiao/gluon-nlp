3b95fa0a95ccb231eae84133610245b7a26d9205
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-04 14:26:19 -0700

    Update verify.py

--- scripts/bert/verify.py
+++ scripts/bert/verify.py
@@ -80,7 +80,7 @@ class AnswerVerifyThreshold(object):
         self.classifier.initialize(init=mx.init.Xavier(), ctx=ctx)
         self.loss = gluon.loss.SigmoidBinaryCrossEntropyLoss()
         self.trainer = Trainer(params=self.classifier.collect_params(), optimizer='sgd',
-                  optimizer_params={'learning_rate': 0.1})
+                  optimizer_params={'learning_rate': 0.3})
         self.accuracy = mx.metric.Accuracy()
         self.f1 = mx.metric.F1()
 
