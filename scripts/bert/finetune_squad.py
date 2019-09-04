6fab94f799d4774c34e041f48a08b8f61f29bbc9
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-04 14:48:49 -0700

    smaller makes easier

--- scripts/bert/finetune_squad.py
+++ scripts/bert/finetune_squad.py
@@ -436,7 +436,7 @@ if version_2:
 else:
     train_data = SQuAD(segment, version='1.1')
 if args.debug:
-    sampled_data = [train_data[i] for i in range(1000)] # 1000 # 120 # 60
+    sampled_data = [train_data[i] for i in range(120)] # 1000 # 120 # 60
     train_data = mx.gluon.data.SimpleDataset(sampled_data)
 log.info('Number of records in Train data:{}'.format(len(train_data)))
 
