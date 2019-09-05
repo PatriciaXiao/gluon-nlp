2a1ded868819b2e829d5be52744b146c6a6e9477
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-04 19:35:32 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -338,7 +338,7 @@ class BertForQA(Block):
             end_hat = self.flatten(self.predict_end(nd.concat(M_0, M_2, dim=-1)))
             predicted_begin = mask_logits(begin_hat, context_mask)
             predicted_end = mask_logits(end_hat, context_mask)
-            print(predicted_begin)
+            print(predicted_begin.as_in_context(mx.cpu()))
             print(predicted_end.shape)
             exit(0)
             return predicted_begin, predicted_end
