baeddf3dd970c3fefc934b1f7e31a7c806043aab
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-04 19:45:07 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -339,9 +339,9 @@ class BertForQA(Block):
             predicted_begin = mask_logits(begin_hat, context_mask)
             predicted_end = mask_logits(end_hat, context_mask)
             prediction = nd.stack(predicted_begin, predicted_end, axis=2)
-            print(prediction.shape)
-            exit(0)
-            return predicted_begin, predicted_end
+            # print(prediction.shape) # (12, 384, 2)
+            # exit(0)
+            return prediction
             # how about doing it again?
             '''
             attended_output_, attended_query_ = self.co_attention_(context_emb_encoded, query_emb_encoded, 
