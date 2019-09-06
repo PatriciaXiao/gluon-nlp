6e2b4c9930461ee942d08819e8e828d9e81c65a6
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-05 18:51:35 -0700

    Update finetune_squad.py

--- scripts/bert/finetune_squad.py
+++ scripts/bert/finetune_squad.py
@@ -615,7 +615,7 @@ def train():
             valid_sequence_length = (1 - token_types).sum(axis=1)
             # sep_mask_1[[0,0],[0,1]] = 1 # it works this way
             sep_index1 = mx.nd.array([[0,0],[0,1]])
-            sep_mask_1[*sep_index1] = 1
+            sep_mask_1[*[idx for idx in sep_index1]] = 1
             print(sep_mask_1)
             # forward and backward
             with mx.autograd.record():
