5af1a67aa70effa9eacdd7ae33ec4d644da5cfbc
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-06 14:13:50 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -322,9 +322,9 @@ class BertForQA(Block):
             if self.qanet_style_out:
                 M = self.project(attended_output)
                 M = self.dropout(M)
-                M_0, _ = self.model_encoder(M, mask=context_mask)
-                M_1, _ = self.model_encoder(M_0, mask=context_mask)
-                M_2, _ = self.model_encoder(M_1, mask=context_mask)
+                M_0, _ = self.model_encoder(M, context_mask)
+                M_1, _ = self.model_encoder(M_0, context_mask)
+                M_2, _ = self.model_encoder(M_1, context_mask)
                 print(attended_output)
                 exit(0)
                 begin_hat = self.flatten(
