3833bca305885b561909f23551d43b374b9a97b3
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-05 19:05:48 -0700

    Update finetune_squad.py

--- scripts/bert/finetune_squad.py
+++ scripts/bert/finetune_squad.py
@@ -616,13 +616,13 @@ def train():
             # sep_mask_1[[0,0],[0,1]] = 1 # it works this way
             for i in range(batch_size):
                 sep_mask_1[i, valid_query_length[i] - 1] = 1.
-                sep_mask_2[i, valid_length[i] - 1]
+                sep_mask_2[i, valid_length[i] - 1] = 1.
             print(sep_mask_1[0])
             print(sep_mask_2[0])
             for j in range(len(sep_mask_1)):
-                if sep_mask_1[0, j] == 1:
+                if int(sep_mask_1[0, j]) == 1:
                     print("sep 1 is at {}".format(j))
-                if sep_mask_2[0, j] == 1:
+                if int(sep_mask_2[0, j]) == 1:
                     print("sep 2 is at {}".format(j))
             # forward and backward
             with mx.autograd.record():
