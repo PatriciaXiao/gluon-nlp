e37282c1328b36c7cfc8625e6a5e35358acad1b9
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-06 17:27:23 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -281,13 +281,10 @@ class BertForQA(Block):
         warpped_out = BilinearSampler(data_raw, grid)
         result = mx.ndarray.squeeze(warpped_out, axis=0)
         # mask shifting
-        print(mx.ndarray.expand_dims(mx.ndarray.expand_dims(mask, 0), 0).shape)
-        print(mx.ndarray.expand_dims(mask, (0, 1)).shape)
-        exit(0)
         mask = mx.ndarray.expand_dims(mx.ndarray.expand_dims(mask, 0), 0)
         mask_result = BilinearSampler(mask, grid)
         mask_result = mx.ndarray.squeeze(mask_result, axis=(0, 1))
-        return result
+        return result, mask_result
 
     def forward(self, inputs, token_types, valid_length=None, additional_masks=None):  # pylint: disable=arguments-differ
         """Generate the unnormalized score for the given the input sequences.
@@ -333,7 +330,7 @@ class BertForQA(Block):
                 raw_offset_query = mx.nd.ones(inputs.shape).as_in_context(inputs.context)
             # use raw_offset to shift the query, and shift back as well, as long as it is permitted
             query, query_mask = self.shift_ndarray(o, query_mask, raw_offset_query)
-            print(query_shifted)
+            print(query, query_mask)
             exit(0)
             context_max_len = bert_output.shape[1] # int(context_mask.sum(axis=1).max().asscalar())
             query_max_len = bert_output.shape[1] # int(query_mask.sum(axis=1).max().asscalar())
