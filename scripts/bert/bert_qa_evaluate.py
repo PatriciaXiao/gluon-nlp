5d43806e5c204e7e3607b6882a0b060b108c649d
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-07 18:24:27 -0700

    Update bert_qa_evaluate.py

--- scripts/bert/bert_qa_evaluate.py
+++ scripts/bert/bert_qa_evaluate.py
@@ -192,6 +192,8 @@ def predict(features,
         print(shift)
         print(orig_start_indexes)
         print(orig_end_indexes)
+        print(feature.orig_answer_text)
+        print([feature.doc_tokens[i] for i in orig_start_indexes])
         exit(0)
 
 
