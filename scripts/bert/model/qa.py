7d28b5416cbf27b7599e6d31a41bd4464350412f
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-09 11:55:43 -0700

    debug

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -338,9 +338,9 @@ class BertForQA(Block):
             context_mask = context_mask[:,:context_max_len]
             query_mask = query_mask[:,:query_max_len]
 
-            print(bert_output[0])
-            print(query[0])
-            print(contx[0])
+            print(bert_output[0,:,0])
+            print(query[0,:,0])
+            print(contx[0,:,0])
             print(query_mask[0])
             print(context_mask[0])
             exit(0)
