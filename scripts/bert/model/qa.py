c349cda0da13ebfa9be4f2e6c1f9e52efce3da41
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-10 15:07:21 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -212,7 +212,7 @@ class BertForQA(Block):
                         weight_initializer=Xavier(),
                         prefix='projection_'
                     )
-                    self.dropout = gluon.nn.Dropout(0.0) # 0.1
+                    self.dropout = gluon.nn.Dropout(0.0) # 0.1 - from the debug set seems that dropout 0.1 is beneficial
                     self.model_encoder = TransformerEncoder(units=bert_out_dim)
                     self.predict_begin = gluon.nn.Dense(
                         units=1,
