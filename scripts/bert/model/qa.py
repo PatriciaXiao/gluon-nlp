19c8e0cb44db2dc864f14607455ab03e22129c26
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-09-09 17:26:15 -0700

    Update qa.py

--- scripts/bert/model/qa.py
+++ scripts/bert/model/qa.py
@@ -406,8 +406,10 @@ class BertForQA(Block):
                 # print(predicted_begin)
                 # exit(0)
                 prediction = nd.stack(predicted_begin, predicted_end, axis=2)
-                print(prediction)
-                exit(0)
+                cls_emb_encoded = mx.ndarray.expand_dims(bert_output[:, 0, :], 1)
+                cls_reshaped = self.cls_mapping(cls_emb_encoded)
+                output = mx.ndarray.concat(cls_reshaped, prediction, dim=1)
+                return (output, bert_output)
         if self.apply_self_attention:
             attended_output, att_weights = self.multi_head_attention(bert_output, bert_output)   
         if self.apply_transformer:
